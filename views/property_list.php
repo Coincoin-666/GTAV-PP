09ff6dda2449297b85dc131bb7db074f95f73596
Coincoin-666 <olf_hrzg@live.fr>
2021-10-11 00:46:32 +0200

    [Project]last one – whats the point of adding pics and not use them. huh ? HUH ? dumbass.

--- views/property_list.php
+++ views/property_list.php
@@ -65,14 +65,19 @@ require "../controllers/pics_ctrl.php";
                                         <div class="col-6">
                                             <figure class="figure properties">
                                                 <img src="<?= $img_path_array['7']['0'] ?>" class="figure-img img-fluid rounded" alt="DowntownCabCo front sign">
-                                                <figcaption class="figure-caption">Smoke On The Water – Cannabis Dispensary</figcaption>
+                                                <figcaption class="figure-caption">Smoke On The Water</figcaption>
                                             </figure>
                                         </div>
                                         <div class="col-6">
                                             <figure class="figure properties">
-                                                <img src="" class="figure-img img-fluid rounded" alt="Sorry, the developer forgot to save screenshots when buying the property.">
-                                                <figcaption class="figure-caption">Hello world!</figcaption>
+                                                <img src="" class="figure-img img-fluid rounded" alt="Sorry, the developer forgot to save screenshots when buying the property in the game.">
+                                                <figcaption class="figure-caption">Hello Friend.</figcaption>
                                             </figure>
+                                            <p>But, so you can forgive me, here's a gift:</p>
+                                            <iframe src="https://open.spotify.com/embed/track/5MMnwYs0hIxkENRsbkWJ2G?theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                                            <p>And a thought: Music is a VERY important part of the games, and I must say the multiple radio station and their playlists are responsible for a good part of my musical culture.
+                                                <br>So, next section: The Music Of Grand Theft Auto! <span class="text-muted">But not now. Now I don't have time.</span>
+                                            </p>
                                         </div>
                                     </div>
                                     <div class="row">
@@ -85,7 +90,7 @@ require "../controllers/pics_ctrl.php";
                                         </div>
                                         <div class="col-6">
                                             <p>Ideally located in South Los Santos, beach front. The owner might call you to handle some deliveries to the store.</p>
-                                            <p>The name of the store is a reference to a song by Deep Purple, and the whole neighborhood looks a lot like Venice Beach in Los Angeles. <span class="text-muted">Please note that medical use of weed has been legal since 1996 and its adult use since 2016.</span></p>
+                                            <p>The name of the store is a reference to a song by Deep Purple, and the whole neighborhood looks a lot like Venice Beach in Los Angeles.<span class="text-muted">Please note that medical use of weed has been legal in California since 1996 and its adult use since 2016.</span></p>
                                         </div>
                                     </div>
                                 </div>
@@ -97,55 +102,104 @@ require "../controllers/pics_ctrl.php";
 
             <div class="container border-f mb-2">
                 <div class="row g-3">
-                    <h4 class="hand" data-bs-toggle="collapse" href="#inpoundCollapse" role="button" aria-expanded="false" aria-controls="inpoundCollapse">Towing Inpound</h4>
-                    <p>
-                    </p>
-                    <div class="collapse" id="inpoundCollapse">
-                        <div class="card card-body propertyCollapse">
-                            <img src="" alt="">
-                            <p>$500 per vehicle</p>
+                    <h4 class="hand" data-bs-toggle="collapse" href="#LSCcollapse" role="button" aria-expanded="false" aria-controls="LSCcollapse">Los Santos Custom</h4>
+                    <form method="POST" name="LosSantosCustom">
+                        <div class="collapse" id="LSCcollapse">
+                            <div class="card card-body propertyCollapse">
+                                <div class="container">
+                                    <div class="row g-2">
+                                        <div class="col-6">
+                                            <figure class="figure properties">
+                                                <img src="<?= $img_path_array['9']['0'] ?>" class="figure-img img-fluid rounded" alt="LSC front by night">
+                                                <figcaption class="figure-caption">Los Santos Custom – Front. By night. Yeah I need a better picture.</figcaption>
+                                            </figure>
+                                        </div>
+                                        <div class="col-6">
+                                            <figure class="figure properties">
+                                                <img src="<?= $img_path_array['10']['0'] ?>" class="figure-img img-fluid rounded" alt="LSC interior.">
+                                                <figcaption class="figure-caption">Inside LSC</figcaption>
+                                            </figure>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <ul class="list-unstyled">
+                                                <li><i class="fas fa-map-marker-alt me-2"></i>Road 68, Harmony – Center San Andreas</li>
+                                                <li><i class="fas fa-dollar-sign me-2"></i>349,000</li>
+                                                <li><i class="fas fa-dollar-sign me-1"></i><i class="fas fa-sign-in-alt me-2"></i>1,600</li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-6">
+                                            <p>What a not-so-profitable investment you might say?</p>
+                                            <p>Purchasing this store of the Los Santos Custom franchise will allow Franklin to mod all the vehicles he wants for free. So that's a good way to have fun.</p>
+                                            <p>You can also take advantage of this situation by "stealing" one of your friend's car: Michael's or Trevor's but also the ones from Amanda and Tracey.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <?php
-        // }
-        ?>
 
+        <!-- -------------------------------------this is just a separator---------------------------------- -->
 
         <div class="container m-3">
             <h3 class="hand blue">Michael's Properties</h3>
             <div class="container border-m mb-2">
                 <div class="row g-3">
-                    <h4 class="hand" data-bs-toggle="collapse" href="#tivoliCollapse" role="button" aria-expanded="false" aria-controls="tivoliCollapse">Cinema Tivoli</h4>
-                    <p>
-                    </p>
-                    <div class="collapse" id="tivoliCollapse">
-                        <div class="card card-body propertyCollapse">
-                            <img src="" alt="">
-                            <p>Free cine + lots of $$$</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    <h4 class="hand" data-bs-toggle="collapse" href="#tivoliCollapse" role="button" aria-expanded="false" aria-controls="tivoliCollapse">Tivoli Cinema</h4>
+                    <form method="POST" name="Tivoli">
+                        <div class="collapse" id="tivoliCollapse">
+                            <div class="card card-body propertyCollapse">
+                                <div class="container">
+                                    <div class="row g-2">
+                                        <div class="col-6">
+                                            <figure class="figure properties">
+                                                <img src="<?= $img_path_array['8']['0'] ?>" class="figure-img img-fluid rounded" alt="LSC front by night">
+                                                <figcaption class="figure-caption">Tivoli Cinema – Entrance</figcaption>
+                                            </figure>
+                                        </div>
+                                        <div class="col-6">
+                                            <p>No other pictures because the neighborhood was SO crowded that I couldn't have a good shot.</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <ul class="list-unstyled">
+                                                <li><i class="fas fa-map-marker-alt me-2"></i>Corner of Morningwood Bld & Cougar Avenue – Morningwood</li>
+                                                <li><i class="fas fa-dollar-sign me-2"></i>30,000,000</li>
+                                                <li><i class="fas fa-dollar-sign me-1"></i><i class="fas fa-sign-in-alt me-2"></i>142,300</li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-6">
+                                            <p>So, yeah...that's a lot of money, and you probably won't be able to buy it until you've finished the game.</p>
 
-        <div class="container m-3">
-            <h3 class="hand orange">Trevor's Properties</h3>
-            <div class="container border-t mb-2">
-                <div class="row g-3">
-                    <h4 class="hand" data-bs-toggle="collapse" href="#macKenzieCollapse" role="button" aria-expanded="false" aria-controls="macKenzieCollapse">MacKenzie's Airfield</h4>
-                    <div class="collapse" id="macKenzieCollapse">
-                        <div class="card card-body propertyCollapse">
-                            <img src="" alt="">
-                            <p>Weapon smuggling + 7500 by air + 5000 by land</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="container m-3">
+                <h3 class="hand orange">Trevor's Properties</h3>
+                <div class="container border-t mb-2">
+                    <div class="row g-3">
+                        <h4 class="hand" data-bs-toggle="collapse" href="#macKenzieCollapse" role="button" aria-expanded="false" aria-controls="macKenzieCollapse">MacKenzie's Airfield</h4>
+                        <div class="collapse" id="macKenzieCollapse">
+                            <div class="card card-body propertyCollapse">
+                                <img src="" alt="">
+                                <p>Weapon smuggling + 7500 by air + 5000 by land</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </main>
 
 
